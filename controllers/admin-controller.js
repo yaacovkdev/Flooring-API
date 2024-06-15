37d1b929d2b053d7188a1f639089f8a668ecc490
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-06-15 00:06:55 -0700

    multiple cors origins

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -2,12 +2,10 @@ const db = require("knex")(require("../knexfile"));
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
-const JWT_KEY = crypto.randomBytes(32).toString("base64");
+const JWT_KEY = "K1HqhFRJx69Wyi0CHN9GffJgJC0tbhms/tvIv+aZOW8=";
 
-const authorize = (req, res, next) => {
+const authenticate = (req, res, next) => {
   const token = req.cookies.token;
-  console.log(req.cookies);
-
   if (!token) {
     return res.status(401).json({ message: "No JWT provided" });
   }
@@ -41,10 +39,9 @@ const login = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: false, // Use true if you are using HTTPS
-        sameSite: "Strict", // Options: 'Strict', 'Lax', 'None'
+        sameSite: "Strict",
       });
-      res.status(200).json({message: "Login Successful"});
-
+      res.status(200).json({ message: "Login Successful" });
     } catch (error) {
       res.status(401).json({
         error: {
@@ -53,28 +50,72 @@ const login = async (req, res) => {
       });
     }
   }
-
-  // If there's an error, send that back to the client
-  // with a 401 status code.
 };
 
-const access = async(req,res) => {
-  console.log("Hi!!");
-}
+const checkStatus = async (req, res) => {
+  res.status(200).json({ message: "Token Approved" });
+};
+
+const getData = async (_req, res) => {
+  try {
+    const responseText = await db("contract_descriptions").select("*");
+    const responseHeroTitles = await db("hero_titles").select("*");
+    const responseImagesController = await db(
+      "public_images_controller"
+    ).select("*");
+
+    const projects_data = [];
+
+    for (let i = 0; i < responseText.length; i++) {
+      const responseImage = await db("public_images_controller")
+        .select("*")
+        .whereLike("dir", `${responseText[i].dir}%`);
+
+      projects_data.push(
+        Object.assign({ images: responseImage }, responseText[i])
+      );
+    }
+    res.status(200).json({
+      hero_titles: responseHeroTitles,
+      images_controller: responseImagesController,
+      projects: projects_data,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Access Failed" });
+  }
+};
 
 //this function is not secured at the moment...
-const setImageDisplayById = async(req,res) => {
+const setImageDisplayById = async (req, res) => {
   try {
-    const response = await db("public_images_controller").where({id: req.params.id}).update({display: req.query.display});
-    res.status(200).json({message: "Database Updated!"});
+    await db("public_images_controller")
+      .where({ id: req.params.id })
+      .update({ display: req.query.display });
+    res.status(200).json({ message: "Database Updated" });
   } catch (error) {
-
+    res.status(404).json({ message: "Error Updating Database" });
   }
-}
+};
+
+const getImageControllerById = async (req, res) => {
+  try {
+    const response = await db("public_images_controller")
+      .select("*")
+      .where({ id: req.params.id });
+
+    res.status(200).json(response[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Image info failed" });
+  }
+};
 
 module.exports = {
-  authorize,
+  authenticate,
   login,
-  access,
-  setImageDisplayById
+  checkStatus,
+  getData,
+  setImageDisplayById,
+  getImageControllerById,
 };
